b2403512b45f04e5b8e7dc48bcf2cd6afb686d39
Evgeni-Kr <krotevgeni1@gmail.com>
2026-04-26 14:43:32 +0300

    HistogramService is used by the HistogramController.

--- src/main/java/org/example/hotels/controller/HistogramController.java
+++ src/main/java/org/example/hotels/controller/HistogramController.java
@@ -1,5 +1,7 @@
 package org.example.hotels.controller;
 
+import org.example.hotels.service.HistogramService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,13 @@ import java.util.Map;
 @RequestMapping("/histogram")
 public class HistogramController {
 
+    private final HistogramService histogramService;
+    @Autowired
+    public HistogramController(HistogramService histogramService) {
+        this.histogramService = histogramService;
+    }
     @GetMapping("/{param}")
     public Map<String, Long> histogram(@PathVariable String param) {
-        return null;
+        return histogramService.createHistogram(param);
     }
 }
