59116f6b82536bf915afe0dd85b1afee9b261166
Evgeni-Kr <krotevgeni1@gmail.com>
2026-04-26 14:36:35 +0300

    Histogram added

--- src/main/java/org/example/hotels/controller/HistogramController.java
+++ src/main/java/org/example/hotels/controller/HistogramController.java
@@ -1,4 +1,18 @@
 package org.example.hotels.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/histogram")
 public class HistogramController {
+
+    @GetMapping("/{param}")
+    public Map<String, Long> histogram(@PathVariable String param) {
+        return null;
+    }
 }
--- src/main/java/org/example/hotels/repository/HotelRepository.java
+++ src/main/java/org/example/hotels/repository/HotelRepository.java
@@ -7,23 +7,57 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 
     @Query("""
-        SELECT DISTINCT h FROM Hotel h
-        LEFT JOIN h.amenities a
-        WHERE (:name     IS NULL OR LOWER(h.name)            LIKE LOWER(CONCAT('%', :name,    '%')))
-          AND (:brand    IS NULL OR LOWER(h.brand)           LIKE LOWER(CONCAT('%', :brand,   '%')))
-          AND (:city     IS NULL OR LOWER(h.address.city)    LIKE LOWER(CONCAT('%', :city,    '%')))
-          AND (:country  IS NULL OR LOWER(h.address.country) LIKE LOWER(CONCAT('%', :country, '%')))
-          AND (:amenityCount = 0  OR a.name IN :amenities)
-    """)
-    List<Hotel> findByFilters( @Param("name")String name,
-                               @Param("brand")String brand,
-                               @Param("city")String city,
-                               @Param("country")String country,
-                               @Param("amenities")List<String> amenities,
-                               @Param("amenityCount") int amenityCount);
+                SELECT DISTINCT h FROM Hotel h
+                LEFT JOIN h.amenities a
+                WHERE (:name     IS NULL OR LOWER(h.name)            LIKE LOWER(CONCAT('%', :name,    '%')))
+                  AND (:brand    IS NULL OR LOWER(h.brand)           LIKE LOWER(CONCAT('%', :brand,   '%')))
+                  AND (:city     IS NULL OR LOWER(h.address.city)    LIKE LOWER(CONCAT('%', :city,    '%')))
+                  AND (:country  IS NULL OR LOWER(h.address.country) LIKE LOWER(CONCAT('%', :country, '%')))
+                  AND (:amenityCount = 0  OR a.name IN :amenities)
+            """)
+    List<Hotel> findByFilters(@Param("name") String name,
+                              @Param("brand") String brand,
+                              @Param("city") String city,
+                              @Param("country") String country,
+                              @Param("amenities") List<String> amenities,
+                              @Param("amenityCount") int amenityCount);
+
+
+    default Map<String, Long> countHotelByBrand(String brand){
+        return toMap(countByBrand(brand));
+    }
+    default Map<String, Long> countHotelByCity(String city){
+        return toMap(countByCity(city));
+    }
+    default Map<String, Long> countHotelByCountry(String country){
+        return toMap(countByCountry(country));
+    }
+    default Map<String, Long> countHotelByAmenities(String amenity){
+        return toMap(countByAmenity(amenity));
+    }
+
+    private Map<String, Long> toMap(List<Object[]> list){
+    return list.stream()
+            .collect(Collectors.toMap(
+                    r->(String) r[0],
+                    r -> (Long) r[1]
+            ));
+    }
+    @Query("SELECT h.brand, COUNT(h) FROM Hotel h GROUP BY h.brand")
+    List<Object[]> countByBrand(String brand);
+    @Query("SELECT h.address.city, COUNT(h) FROM Hotel h GROUP BY h.address.city")
+    List<Object[]> countByCity(String city);
+    @Query("SELECT h.address.country, COUNT(h) FROM Hotel h GROUP BY h.address.country")
+    List<Object[]> countByCountry(String country);
+    @Query("SELECT a.name, COUNT(h) FROM Hotel h JOIN h.amenities a GROUP BY a.name")
+    List<Object[]> countByAmenity(String amenity);
+
 }
